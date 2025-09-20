5d1686f63b27e63b5a2caddf2dc1b9a871687fce
Mathias Pius <contact@pius.io>
2025-09-20 21:30:08 +0200

    How can bools be so hard man

--- main.ps1
+++ main.ps1
@@ -1,7 +1,9 @@
-param([bool]$IsUpdated=$false)
-param([string]$Command)
+param(
+    [string]$Command,
+    [bool]$IsUpdated=0
+)
 
 if(!$IsUpdated) {
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/MathiasPius/NephewPC/refs/heads/main/main.ps1 -OutFile main.ps1
-    & ".\main.ps1" --IsUpdated=$true
+    & ".\main.ps1" $Command 1
 }
