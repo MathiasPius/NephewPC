f3d9f4bec37d64c546eb106587e246981f61bf73
Mathias Pius <contact@pius.io>
2025-09-20 21:24:00 +0200

    This is already getting out of hand

--- main.ps1
+++ main.ps1
@@ -3,6 +3,5 @@ param([string]$Command)
 
 if(!$IsUpdated) {
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/MathiasPius/NephewPC/refs/heads/main/main.ps1 -OutFile main.ps1
-    & ".\main.ps1"
-
+    & ".\main.ps1" --IsUpdated=$true
 }
